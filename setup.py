33e306301c73acc7981c4d75d855f45eb0343f66
Dennis Fischer <chaosking0912@gmail.com>
2016-09-05 21:54:50 +0200

    Version 1.3

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-	
 setup(
     name='container_decrypter',
-    version='1.2',
+    version='1.3',
     packages=['container_decrypter'],
     url='https://github.com/dennisfischer/container_decrypter',
     license='MIT',
@@ -21,7 +14,6 @@ setup(
 		"requests"
 	],
 	test_suite="tests",
-	long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
