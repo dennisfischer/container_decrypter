e0c9cae6dcff15b05333a271fab69884ba500d73
Dennis Fischer <chaosking0912@gmail.com>
2016-09-05 20:59:15 +0200

    Added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='container_decrypter',
+    version='1.0',
+    packages=['tests', 'container_decrypter'],
+    url='https://github.com/dennisfischer/container_decrypter',
+    license='MIT',
+    author='Dennis Fischer',
+    author_email='dennis.fischer@live.com',
+    description='A library for decrypting JDownloader container formats. Currently it supports only DLC files. '
+)
